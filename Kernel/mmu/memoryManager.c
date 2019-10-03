189399d8b70e6df4fff620bbd5f2befc0eee4dcb
fletterio <fletterio@itba.edu.ar>
2019-10-02 22:42:34 -0300

    xX

--- Kernel/mmu/memoryManager.c
+++ Kernel/mmu/memoryManager.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include "include/null.h"
+#include "../include/null.h"
 
 #define STARTING_MEM_LOCATION 0x600000
 #define INITIAL_MEM_SIZE 100000
@@ -10,7 +10,10 @@ typedef struct memBlock{
     uint64_t size;
 } memBlock;
 
+
+//memSize is changed dinamically to account for the size needed to keep track of the many memory blocks
 static uint64_t memSize;
+static memBlock * greatest;
 
 static void swap(memBlock * block1, memBlock * block2){
     memBlock aux = *block1;
@@ -20,20 +23,39 @@ static void swap(memBlock * block1, memBlock * block2){
     block2->size = aux.size;
 }
 
+//initializeMemory() must be called once before using the memory Manager. Failure to do so will most likely result in segFault
+
 void initializeMemory(){
+
+    //we need to account for the size of the memBlock struct at the end used to represent an "end of array"
     memSize = INITIAL_MEM_SIZE - sizeof(memBlock);
-    memBlock * aux = STARTING_MEM_LOCATION + memSize;
-    aux->startLocation = NULL;
+
+    //the last memBlock in this array has its startLocation set to NULL to signify an end of the array
+    greatest = (memBlock *) (STARTING_MEM_LOCATION + memSize);
+    greatest->startLocation = NULL;
 }
 
+//something like a malloc(). Traverses the memory linearly using a first fit algorithm.
+//Keeps track of different memory blocks with a memBlock "array" at the end, which grows backwards
+
 void * giveMeMemory(uint64_t size){
-    memBlock * lowest = STARTING_MEM_LOCATION + INITIAL_MEM_SIZE - sizeof(memBlock);
+
+    //"lowest" would be the allocated chunk of memory closest to the start of the memory such that the new block we want to allocate 
+    //is closer to the start of the memory than "lowest" 
+    memBlock * lowest = (memBlock *) (STARTING_MEM_LOCATION + INITIAL_MEM_SIZE - sizeof(memBlock));
+
+    //"location" is the current starting point for a new chunk of memory
     uint64_t location = STARTING_MEM_LOCATION;
 
     while(lowest->startLocation != NULL){
 
-        if(lowest->startLocation - location >= size){
+        //chequear tambien espacio suficiente para un nuevo memBlock al final
+
+        if(lowest->startLocation - location >= size && (uint64_t) greatest - (greatest->startLocation + greatest->size) >= sizeof(memBlock) ){
             void * retVal = (void *) location;
+
+            //since the memBlock array is ordered, we need to save the new memBlock where lowest is currently,
+            //and then shift the rest of the array one position 
 
             memBlock aux = *lowest;
             lowest->startLocation = location;
@@ -44,24 +66,37 @@ void * giveMeMemory(uint64_t size){
                 swap(&aux, lowest);
                 lowest -= sizeof(memBlock);
             }
+
+            //add the "end of array" marker and return
             lowest->startLocation = NULL;
             memSize -= sizeof(memBlock);
             return retVal;
-            //return statement
         }
 
         location = lowest->startLocation + lowest->size;
         lowest -= sizeof(memBlock);
 
     }
-    //si llegue aca, entonces no hay memoria entre dos bloques o entre principio y primer bloque
-    if(STARTING_MEM_LOCATION + memSize - location < size){
+
+    //if the program reaches this point, then a "lowest" block does not exist. We check whether there is enough
+    //space between the end of the last allocated chunk and the end of our memory (the end being the last
+    //memBlock struct in our backwards array at the end)
+
+    if(STARTING_MEM_LOCATION + memSize - location < size + sizeof(memBlock)){
+        //there isn't enough space
         return (void *)NULL;
     }
 
+    //we set this chunk of memory as allocated by saving it in the last position of the memBlock array
     lowest->startLocation = location;
     lowest->size = size;
+
+    //decrement memory size to account for the new memBlock created
+    
     memSize -= sizeof(memBlock);
+
+    //indicate the end of the memBlock array
+
     lowest -= sizeof(memBlock);
     lowest->startLocation = NULL;
     return (void *)location;
