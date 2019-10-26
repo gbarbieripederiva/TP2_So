3027815718e6f853d1eff1dd25b7932ed495673e
franciscochoi <fchoi@itba.edu.ar>
2019-10-26 17:08:35 -0300

    fewqwe

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,10 +8,12 @@
 #include <schedulerTest.h>
 
 int main() {
-    uint64_t testInfo = sys_create_process("test", 1, (uint64_t) test);
-    sys_run_process(testInfo, READY);
-    //uint64_t terminalInfo;
-    //terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
-    //sys_run_process(terminalInfo, 1);
+    //uint64_t testInfo = sys_create_process("test", 1, (uint64_t) test);
+    //sys_run_process(testInfo, READY);
+    uint64_t terminalInfo;
+    terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
+    sys_run_process(terminalInfo, 1);
     return 0;
 }
+
+
