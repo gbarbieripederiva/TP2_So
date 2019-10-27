32ba6b0f200998157e8a450825cca0518d4bdbc4
fletterio <fletterio@itba.edu.ar>
2019-10-27 17:02:46 -0300

    push

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -14,7 +14,7 @@ void checkTicks(){
 
 int main() {
     uint64_t terminalInfo;
-    terminalInfo = sys_create_process("terminal",0, (uint64_t)checkTicks);
+    terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
     sys_run_process(terminalInfo, 1);
     return 0;
 }
