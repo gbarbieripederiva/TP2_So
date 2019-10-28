b5ef035ed44d54ce9aa27e46ee4ef3ebe5719a1f
franciscochoi <fchoi@itba.edu.ar>
2019-10-28 15:24:55 -0300

    feqwewq

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,7 +8,7 @@
 
 void checkTicks(){
     while(1){
-        printDec(getTicks());
+        print("hola");
     }
 }
 
@@ -16,6 +16,9 @@ int main() {
     uint64_t terminalInfo;
     terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
     sys_run_process(terminalInfo, 1);
+
+    uint64_t chekInfo = sys_create_process("check", 1, checkTicks);
+    sys_run_process(chekInfo, 1);
 
 
     return 0;
