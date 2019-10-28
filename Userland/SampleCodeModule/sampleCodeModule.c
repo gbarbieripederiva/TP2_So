7d2906ee128e900804d43f393a472de6fe570b49
franciscochoi <fchoi@itba.edu.ar>
2019-10-28 16:27:11 -0300

    fewqfewq

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -5,12 +5,23 @@
 #include <terminal.h>
 #include <jueguito.h>
 #include <libasm.h>
+#include <naiveConsole.h>
+
+void checking(){
+    while(1){
+        ncPrint("Hola");
+        ncNewLine();
+    }
+}
 
 int main() {
     uint64_t terminalInfo;
     terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
     sys_run_process(terminalInfo, 1);
-    
+
+    uint64_t checkInfo = sys_create_process("checking", 1, (uint64_t) checking);
+    sys_run_process(checkInfo, 1);
+
     return 0;
 }
 
