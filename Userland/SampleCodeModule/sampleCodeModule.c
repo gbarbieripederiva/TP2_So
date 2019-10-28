726d3471557d68ce9a9ca7f4564a038a4b64a9a3
franciscochoi <fchoi@itba.edu.ar>
2019-10-28 15:56:47 -0300

    afewqeqw

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -6,20 +6,11 @@
 #include <jueguito.h>
 #include <libasm.h>
 
-void checkTicks(){
-    while(1){
-        print("hola");
-    }
-}
-
 int main() {
     uint64_t terminalInfo;
     terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
     sys_run_process(terminalInfo, 1);
-
-    uint64_t chekInfo = sys_create_process("check", 1, checkTicks);
-    sys_run_process(chekInfo, 1);
-
-
+    
     return 0;
 }
+
