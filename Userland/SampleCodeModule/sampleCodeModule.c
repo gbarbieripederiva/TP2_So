ec5b733940fc22607f50f731a582bab697983954
franciscochoi <fchoi@itba.edu.ar>
2019-10-30 15:04:01 -0300

    deqwewq

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -13,6 +13,7 @@ void checking(){
        printAction(0);
        print("Chau");
        printAction(0);
+
        int sid;
        sid = sys_create_semaphore("hola", 1);
        sys_sem_wait(sid);
@@ -20,9 +21,9 @@ void checking(){
 }
 
 int main() {
-    uint64_t terminalInfo;
-    terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
-    sys_run_process(terminalInfo, 1);
+    //uint64_t terminalInfo;
+    //terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
+    //sys_run_process(terminalInfo, 1);
 
     uint64_t checkInfo = sys_create_process("checking", 1, (uint64_t) checking);
     sys_run_process(checkInfo, 1);
