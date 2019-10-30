ddc147cf460471d1b4003152df993ea65d704cc1
franciscochoi <fchoi@itba.edu.ar>
2019-10-30 15:06:55 -0300

    afasew

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -21,12 +21,13 @@ void checking(){
 }
 
 int main() {
-    //uint64_t terminalInfo;
-    //terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
-    //sys_run_process(terminalInfo, 1);
+    uint64_t terminalInfo;
+    terminalInfo = sys_create_process("terminal",0, (uint64_t)terminal);
+    sys_run_process(terminalInfo, 1);
 
     uint64_t checkInfo = sys_create_process("checking", 1, (uint64_t) checking);
     sys_run_process(checkInfo, 1);
+    sys_sem_post(1);
 
     
 
