d2c137e2dee26c696eca9346189f9b61af1ff7b7
franciscochoi <fchoi@itba.edu.ar>
2019-11-02 18:26:52 -0300

    fewq

--- Userland/SampleCodeModule/terminal.c
+++ Userland/SampleCodeModule/terminal.c
@@ -609,6 +609,7 @@ void handleCommand(){
     char potentialCommand[MAX_COMDESC];
     strncopy(terminalBuffer, potentialCommand, bufferSize);
     pipesAmount = pipesCount(potentialCommand);
+    printDec(pipesAmount);
     tokens = tokensCounter(potentialCommand);
     while(tokens + 1 > tokenIterator){ //consumes all the tokens the potential command has
         handleToken(potentialCommand, tokenIterator);
