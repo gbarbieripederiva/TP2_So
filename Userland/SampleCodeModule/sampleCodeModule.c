841fced7a84f0db4ba697898a5835821f4f1c273
Guido Barbieri <gbarbieripederiva@gmail.com>
2019-11-04 18:43:57 -0300

    asdasd

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -7,24 +7,35 @@
 #include <libasm.h>
 
 
-void checking(){
-    
-    int sid = sys_create_semaphore(12345678, SEM_LOCKED);
-    printDec(sid);
-    printAction(0);
-    sys_sem_post(sid);
-    sys_sem_close(sid);
-}
+// void checkingIn(){
+//     for(int i=0;i<10;i++){
+//         printDec(i);
+//     }
+// }
+
+// void checkingOut(){
+//     char str[2];
+//     str[1]=0;
+//     while(1){
+//         str[0]=getLastInput();
+//         print(str);
+//     }
+// }
 
 int main() {
     uint64_t terminalInfo;
     terminalInfo = sys_create_process(1, (uint64_t)terminal);
     sys_run_process(terminalInfo, 1);
-    
-    uint64_t checkInfo = sys_create_process(1, (uint64_t)checking);
-    sys_run_process(checkInfo, 1);
 
-    
+    // uint64_t in=sys_create_process(1,(uint64_t)checkingIn);
+    // uint64_t out=sys_create_process(1,(uint64_t)checkingOut);
+    // int pipe[2];
+    // sys_open_pipe(pipe);
+    // sys_dup_in(((processInfo)in)->pid,pipe[0]);
+    // sys_dup_in(((processInfo)in)->pid,pipe[1]);
+    // sys_run_process(in,1);
+    // sys_run_process(out,1);
+
 
     return 0;
 }
