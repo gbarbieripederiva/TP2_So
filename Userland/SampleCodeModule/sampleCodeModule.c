428c8be3daef94ded91f6e5a13878fe2fbb717f5
franciscochoi <fchoi@itba.edu.ar>
2019-11-06 00:06:47 -0300

    fewq

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -22,7 +22,7 @@ int main() {
     uint64_t terminalInfo;
     printDec(((processInfo) terminalInfo) -> pid);
     terminalInfo = sys_create_process(1, (uint64_t)terminal);
-    sys_set_stdin((terminalInfo, fd);
+    sys_set_stdin(terminalInfo, fd);
     sys_run_process(terminalInfo, 1);
 
     uint64_t checkInfo = sys_create_process(0, (uint64_t)checking);
