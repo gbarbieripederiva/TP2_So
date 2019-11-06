bfb09258199e6d39d308a0d6639565bdb40d0887
franciscochoi <fchoi@itba.edu.ar>
2019-11-05 21:18:54 -0300

    fewq

--- Kernel/fd.c
+++ Kernel/fd.c
@@ -63,6 +63,7 @@ int give_me_fd(int name){
         while(j < FD_AMOUNT && fds[j] != NULL){
             j++;
         }
+        ncPrint("HOLA");
 
         if(j == FD_AMOUNT){
             return -1;
