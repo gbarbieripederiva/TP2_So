ab016ba5689cdb03f487dfaf2fe275ae141c525f
franciscochoi <fchoi@itba.edu.ar>
2019-11-06 17:41:59 -0300

    fewq

--- Userland/SampleCodeModule/terminal.c
+++ Userland/SampleCodeModule/terminal.c
@@ -868,17 +868,10 @@ void catCommand(uint64_t string){//dont know if it should be redirected
         int stdin = sys_get_stdin();
         char buff[100] = {0};
         int stdout = sys_get_stdout();
-        printDec(stdout);
-        //sys_write_pipe(stdin, (char *)string, strlength((char *) string) + 1);
-        if(string == 0){
+        write_fd(stdout, (char *) string);
         sys_read_pipe(stdin, buff, 100);
-        write_fd(stdout, buff);}
-        else{
-            print("ACA TAMBIEN");
-            print(string);
-            write_fd(stdout, (char *)string);
-        }
-        
+        write_fd(stdout, buff);
+       
         
 }
 
