ba66084396d9077cac74dfc7c414adafcb4e8aab
franciscochoi <fchoi@itba.edu.ar>
2019-11-06 13:42:35 -0300

    fdqw

--- Kernel/fd.c
+++ Kernel/fd.c
@@ -163,7 +163,6 @@ int fd_read(int fd, char *dest , int maxSize){
     int j = 0;
 
     while(j < maxSize && fds[pos] -> count != 0){
-        ncPrint("hola");
         dest[j] = fds[pos] -> buffer[fds[pos] -> read_index % BUFFER_SIZE];
         (fds[pos] -> read_index) ++;
         (fds[pos] -> count)--;
