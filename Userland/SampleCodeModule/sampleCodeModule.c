e8b586693a1f20e236729d8472a58ee6b11bb573
franciscochoi <fchoi@itba.edu.ar>
2019-11-06 00:20:46 -0300

    fewq

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,10 +8,11 @@
 
 
 void checking(){
-    
+    /*
     int fd = sys_open_pipe(1);
     char *str = "HOLA MUNDO";
     sys_write_pipe(fd, str, strlength(str) + 1);
+    */
     
 
     
@@ -21,7 +22,7 @@ int main() {
     int fd = sys_open_pipe(1);
     uint64_t terminalInfo;
     terminalInfo = sys_create_process(1, (uint64_t)terminal);
-    //sys_set_stdin(terminalInfo, fd);
+    sys_set_stdin(terminalInfo, fd);
     sys_run_process(terminalInfo, 1);
 
     uint64_t checkInfo = sys_create_process(0, (uint64_t)checking);
